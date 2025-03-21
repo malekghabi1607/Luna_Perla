df21cfde1a2517657bd478be6aa7a07fd8046ed8
Malek GHABI <145768940+malekghabi1607@users.noreply.github.com>
2025-03-21 03:23:54 +0000

    Ajout du contrôleur notre-histoire.php

---
+++ control/notre-histoire.php
@@ -0,0 +1,9 @@
+<?php
+$racine_path = '../';  // Ajuste le chemin si nécessaire
+$titre = 'À Propos de Luna Perla';
+
+// Inclure le header et le template
+include($racine_path . "templates/front/header.php");
+include($racine_path . "templates/front/notre-histoire.php");
+include($racine_path . "templates/front/footer.php");
+?>
