c1ba5150081204eb566c24e6a29fe13554342b7b
Malek GHABI <145768940+malekghabi1607@users.noreply.github.com>
2025-03-21 03:24:52 +0000

    Ajout du contrôleur soldes.php

---
+++ control/soldes.php
@@ -0,0 +1,14 @@
+<?php
+$racine_path = '../';
+$titre = 'Soldes';
+
+
+
+
+
+
+// Inclure le header et le template
+include($racine_path . "templates/front/header.php");
+include($racine_path . "templates/front/soldes.php");
+include($racine_path . "templates/front/footer.php");
+?>
