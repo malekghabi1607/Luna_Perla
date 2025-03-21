7187d22d3d72a7faa9ee6939e223003827850908
Malek GHABI <145768940+malekghabi1607@users.noreply.github.com>
2025-03-21 03:29:33 +0000

    Ajout du fichier compte.php pour la gestion des comptes utilisateurs

---
+++ templates/front/compte.php
@@ -0,0 +1,9 @@
+<main>
+    <div class="welcome-container">
+        <h1>Bienvenue sur votre espace personnel</h1>
+        <p>Ceci est votre espace personnel.</p>
+        <p>Vous pouvez gérer vos informations personnelles.</p>
+
+        <a href="login.php" class="logout-btn">Se déconnecter</a>
+    </div>
+</main>
