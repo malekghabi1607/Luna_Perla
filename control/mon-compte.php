f95720c073c664ad240fdd344a36157c47a89c92
Malek GHABI <145768940+malekghabi1607@users.noreply.github.com>
2025-03-21 03:23:49 +0000

    Ajout du contrôleur mon-compte.php

---
+++ control/mon-compte.php
@@ -0,0 +1,13 @@
+<?php
+$titre = 'Mon compte';
+$racine_path = '../'; // Adapter à votre arborescence
+
+$titre = 'Mon compte';
+$racine_path = '../'; // Adapter à votre arborescence
+
+
+
+include($racine_path . 'templates/front/header.php');
+include($racine_path . 'templates/front/compte.php') ;
+include($racine_path . 'templates/front/footer.php');
+?>
