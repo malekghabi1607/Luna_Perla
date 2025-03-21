fcbc7cca6b4102f7dbceeb7f069f1371be5d0383
Ghabi malek <malekghabi129@gmail.com>
2025-03-21 02:00:27 +0100

    Ajout et mise à jour du fichier index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$titre = 'Accueil';
+$racine_path = './';
+?>
+
+<?php include($racine_path . 'templates/front/header.php'); ?>
+
+<?php include($racine_path . 'templates/front/main.php'); ?>
+
+<?php include($racine_path . 'templates/front/footer.php'); ?>
