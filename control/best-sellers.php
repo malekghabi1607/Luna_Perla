530bb58e4989a653bfdc093746e9b60ac4966d0f
Malek GHABI <145768940+malekghabi1607@users.noreply.github.com>
2025-03-21 03:22:52 +0000

    Ajout du contrôleur best-sellers.php

---
+++ control/best-sellers.php
@@ -0,0 +1,13 @@
+<?php
+$racine_path = '../';
+$titre = 'best-sellers';
+
+
+
+
+
+// Inclure le header et le template
+include($racine_path . "templates/front/header.php");
+include($racine_path . "templates/front/best-sellers.php");
+include($racine_path . "templates/front/footer.php");
+?>
