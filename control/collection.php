fdd903ab4400c547d630f3614dbbc240b0065427
Malek GHABI <145768940+malekghabi1607@users.noreply.github.com>
2025-03-31 20:54:04 +0000

    Modifier collection.php

--- control/collection.php
+++ control/collection.php
@@ -2,7 +2,8 @@
 $racine_path = '../';
 $titre = 'Collection';
 
-
+// 1ere partie du projet
+/*
 // Définition de la liste des produits
 $products = [
     ["id" => 1, "name" => "Collier Sautoir - JOANA", "price" => "26", "image" => "images/collier-joana-1.jpg"],
@@ -10,9 +11,35 @@ $products = [
 
 
 ];
+*/
 
-// Inclure le header et le template
+// 2eme partie du projet
+// On inclut le modèle
 include($racine_path . "templates/front/header.php");
-include($racine_path . "templates/front/collection.php");
+echo '<main>
+    <section class="collection-container">
+    <div class="product-grid">';
+
+require($racine_path . 'model/Produit_lp.php');
+require($racine_path . 'model/Images_lp.php');
+use bd\Produit_lp;
+use bd\ImagesProduitLP;
+
+$produitBD = new Produit_lp();
+foreach ($produitBD->listProduits() as $produit) {
+    $images=new ImagesProduitLP();
+    $id_produit = $produit->id;
+    $tableauimages=$images->fetchByProduitId($id_produit);
+    $nom_produit = $produit->specific_name;
+    $prix_produit = $produit->prix;
+    $stock_produit = $produit->stock;
+    $disponibilite_produit = $produit->availability ? "En stock" : "Rupture";
+    $lien_fiche_produit = $racine_path . "control/bijou.php?id=$id_produit";
+    include($racine_path . "templates/front/carte_produit.php");
+}
+echo '  </div>
+</section>
+</main>
+';
 include($racine_path . "templates/front/footer.php");
 ?>
