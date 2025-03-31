d2f7f30c71b5a301f3b0ad4fc15a1a87a420c1b4
Malek GHABI <145768940+malekghabi1607@users.noreply.github.com>
2025-03-31 20:54:05 +0000

    Modifier collection.php (template front)

--- templates/front/collection.php
+++ templates/front/collection.php
@@ -1,32 +1,24 @@
-<main>
-    <section class="collection-container">
+
         <h1>Collection</h1>
         <p class="collection-description">Découvrez notre gamme de bijoux élégants et raffinés.</p>
-
-        <!-- 📌 Liste des produits -->
-
-        <div class="product-grid">
             <?php if (isset($products) && is_array($products)): ?>
-                        <?php foreach ($products as $product): ?>
-                            <div class="product-card" data-price="<?php echo $product["price"]; ?>">
-                                <a href="../control/bijou.php?id=<?php echo $product["id"]; ?>">
-                                    <img src="<?php echo $racine_path . "templates/front/" . $product["image"]; ?>"
-                                        alt="<?php echo $product["name"]; ?>">
-                                </a>
-                                <h3>
-                                    <a href="../control/bijou.php?id=<?php echo $product["id"]; ?>">
-                                        <?php echo $product["name"]; ?>
-                                    </a>
-                                </h3>
-                                <p><?php echo $product["price"]; ?>€</p>
-                                <a href="../control/bijou.php?id=<?php echo $product["id"]; ?>" class="btn-details">Voir le bijou</a>
-                            </div>
-                        <?php endforeach; ?>
-                    <?php else: ?>
-                        <p>Aucun produit disponible.</p>
-                    <?php endif; ?>
-                </div>
-    </section>
-</main>
-
+                <?php foreach ($products as $product): ?>
+                    <div class="product-card" data-price="<?= htmlspecialchars($product["prix"]) ?>">
+                        <a href="../control/bijou.php?id=<?= $product["id"] ?>">
+                            <img src="<?= $racine_path . htmlspecialchars($product["image"]) ?>"
+                                 alt="<?= htmlspecialchars($product["specific_name"]) ?>">
+                        </a>
+                        <h3>
+                            <a href="../control/bijou.php?id=<?= $product["id"] ?>">
+                                <?= htmlspecialchars($product["specific_name"]) ?>
+                            </a>
+                        </h3>
+                        <p><?= htmlspecialchars($product["prix"]) ?>€</p>
+                        <a href="../control/bijou.php?id=<?= $product["id"] ?>" class="btn-details">Voir le bijou</a>
+                    </div>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <p>Aucun produit disponible.</p>
+            <?php endif; ?>
+  
 <script src="../js/script.js"></script>
